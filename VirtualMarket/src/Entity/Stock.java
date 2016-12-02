8eb63e82d3e58b5652d867bbdc5cb7d8bbd927f9
Sanjay Bansal <sanjay.bansal57@gmail.com>
2016-12-02 14:05:56 +0530

    comit

--- VirtualMarket/src/Entity/Stock.java
+++ VirtualMarket/src/Entity/Stock.java
@@ -1,5 +1,6 @@
 package Entity;
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class Stock {
 	private String name;
@@ -7,20 +8,53 @@ public class Stock {
 	private double low;
 	private double close;
 	private BigDecimal volumn;
+	private BigDecimal totalTradedQty;
 	private double open;
+	private Date date;
+	private double last;
 	
-	public Stock(String name, double high, double low, double close,
-			BigDecimal volumn, double open) {
+	
+
+
+	public Stock(String name, double high, double low, double close, BigDecimal volumn, BigDecimal totalTradedQty,
+			double open, Date date, double last) {
 		this.name = name;
 		this.high = high;
 		this.low = low;
 		this.close = close;
 		this.volumn = volumn;
+		this.totalTradedQty = totalTradedQty;
 		this.open = open;
+		this.date = date;
+		this.last = last;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public BigDecimal getTotalTradedQty() {
+		return totalTradedQty;
+	}
+
+	public void setTotalTradedQty(BigDecimal totalTradedQty) {
+		this.totalTradedQty = totalTradedQty;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getLast() {
+		return last;
+	}
+
+	public void setLast(double last) {
+		this.last = last;
 	}
 
 	public void setName(String name) {
--- VirtualMarket/src/playground/demo.java
+++ VirtualMarket/src/playground/demo.java
@@ -1,48 +1,69 @@
 package playground;
 
 import java.io.IOException;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
+import java.util.List;
+
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+
+
+
+import Parser.Parser;
 
 public class demo {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, DocumentException {
 		String httpsURL = "https://www.quandl.com/api/v3/datasets/NSE/SBIN.xml?start_date=2016-11-28";
-	    
-	    try {
-
-			Client client = Client.create();
-
-			WebResource webResource = client
-			   .resource(httpsURL);
-
-			ClientResponse response = webResource.accept("application/xml")
-	                   .get(ClientResponse.class);
-
-			if (response.getStatus() != 200) {
-			   throw new RuntimeException("Failed : HTTP error code : "
-				+ response.getStatus());
+		Parser parser = new Parser();
+		List<Node> nodes = parser.parsePropertyFile("resources/properties.xml");
+		for (Node node : nodes) {
+			System.out.println(node.getName());
+			System.out.println("\nname of stock :" + node.valueOf("@name"));
+			System.out.println("Exchange name : " + node.valueOf("@exchange"));
+			List<Node> columns=node.selectNodes("./column-names/column-name");
+			for(Node column:columns){
+				System.out.println(column.getStringValue());
 			}
-
-			String output = response.getEntity(String.class);
-
-			System.out.println("Output from Server .... for SBI\n");
-			System.out.println(output);
-			
-			
-			
-			
-			
-
-		  } catch (Exception e) {
-
-			e.printStackTrace();
-
-		  }
-
-		}
 	}
-	  
+	}
 
+}
+	
+	// try {
+	//
+	//
+	// }
 
+	// Client client = Client.create();
+	//
+	// WebResource webResource = client
+	// .resource(httpsURL);
+	//
+	// ClientResponse response = webResource.accept("application/xml")
+	// .get(ClientResponse.class);
+	//
+	// if (response.getStatus() != 200) {
+	// throw new RuntimeException("Failed : HTTP error code : "
+	// + response.getStatus());
+	// }
+	//
+	// String output = response.getEntity(String.class);
+	//
+	// System.out.println("Output from Server .... for SBI\n");
+	// System.out.println(output);
+	//
+	//
+	//
+	//
+	//
+	//
+	// } catch (Exception e) {
+	//
+	// e.printStackTrace();
+	//
+	// }
+
+	// }
+//}
+//
+//}
